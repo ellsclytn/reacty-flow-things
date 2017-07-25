7773845de2a98cfef5d823a381670f90df9d4a9e
Ellis Clayton <ellis@ellisclayton.info>
2017-07-25 16:54:52 +1000

    WIP

--- src/store/actions/prismic.js
+++ src/store/actions/prismic.js
@@ -1,13 +1,30 @@
 // @flow
+import prismic from 'prismic-javascript'
 import type { ThunkAction } from './__types__'
+import {
+  PRISMIC_API_URL,
+  PRISMIC_API_KEY
+} from '../../config/env'
 
 export const FETCH_DOCUMENT_COUNTS_SUCCESS = 'FETCH_DOCUMENT_COUNTS_SUCCESS'
 
 export const addDocumentCounts = (): ThunkAction => (dispatch) => (
-  setTimeout(() => (
+  prismic
+  .getApi(PRISMIC_API_URL, {accessToken: PRISMIC_API_KEY})
+  .then((api) => {
+    return api.query(
+      prismic.Predicates.at('document.type', 'manual-send'),
+      { pageSize: 100 }
+    )
+  }).then((res) => {
+    const { total_results_size: count } = res
+    console.log(res.results.filter((doc) => (
+      doc.data['content-text'].includes('{{%20databag.page.url%')
+    )))
+
     dispatch({
       type: FETCH_DOCUMENT_COUNTS_SUCCESS,
-      count: 1
+      count
     })
-  ), 5000)
+  }).catch((err) => (console.log(err)))
 )
